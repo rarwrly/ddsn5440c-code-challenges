ff914d52b50918f858aca8ee5a5c0985c094e36d
rarwrly <ehc2642@gmail.com>
2020-03-03 18:12:06 -0500

    Exercise 1

---
+++ index-class.js
@@ -0,0 +1,8 @@
+
+/* 1. Write a function that takes three arguments - all numbers - adds them together, divide by 3 and displays the result on the console.*/
+function Numbers(a,b,c){
+		var result = ((c+ b + a) / 3);
+	 	console.log(result) 
+
+}
+
